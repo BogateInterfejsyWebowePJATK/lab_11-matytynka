a679a3fd6200649ef7676bf9e66c268a1e7b57df
matytynka <matytynka@o2.pl>
2021-01-12 17:47:53 +0100

    zad 2 added

--- src/components/randomNumbers.js
+++ src/components/randomNumbers.js
@@ -1,12 +1,27 @@
 import NumberList from "./numberList";
 import RandomGenerator from "./randomGenerator";
+import react from "react";
 
 export default function randomNumbers({size, min, max}) {
-    let randomNums = [size];
 
-    for(let i = 0; i < size; i++) {
-        randomNums[i] = RandomGenerator(min, max)
+    const [numsList, setNumsList] = react.useState([size])
+
+    react.useEffect( () => {
+        generateNumsList()
+    }, [])
+
+    const generateNumsList = () => {
+        let randomNums = [size]
+        for (let i = 0; i < size; i++) {
+            randomNums[i] = RandomGenerator(min, max)
+        }
+        setNumsList(randomNums)
     }
 
-    return<NumberList numbers={randomNums}/>
+    return(
+        <div className="numberList">
+            <NumberList numbers={numsList}/>
+            <button type="button" onClick={generateNumsList}>Odśwież</button>
+        </div>
+    )
 }
